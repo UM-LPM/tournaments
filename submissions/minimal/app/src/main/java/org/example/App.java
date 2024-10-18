923e84beb0557a5e14b7183f894999712859cba3
Žiga Leber <mail+github@zigaleber.com>
2024-10-18 20:31:01 +0200

    Add example algorithm and benchmark

--- submissions/minimal/app/src/main/java/org/example/App.java
+++ submissions/minimal/app/src/main/java/org/example/App.java
@@ -1,11 +1,32 @@
 package org.example;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import org.um.feri.ears.problems.StopCriterionException;
+import org.um.feri.ears.benchmark.RPCOe1Benchmark;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+public class App {
+
+    public static void main(String[] args) throws StopCriterionException {
+        var algorithm = new GWO();
+        var benchmark = new RPCOe1Benchmark();
+        benchmark.initAllProblems(); //manually initialize problems
+
+        var tasks = benchmark.getAllTasks();
+        int numberOfRuns = benchmark.getNumberOfRuns();
+
+        for (var t: tasks) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(algorithm.getAlgorithmInfoCSV()).append(";").append(t.getTaskInfoCSV());
+            sb.append("\n");
+
+            for (int i = 0; i < numberOfRuns; i++) {
+                var result = algorithm.execute(t);
+
+                sb.append(result.getEval());
+                if(i+1 < numberOfRuns)
+                    sb.append("\n");
+                t.resetCounter();
+            }
+            System.out.println(sb);
+        }
     }
 }
